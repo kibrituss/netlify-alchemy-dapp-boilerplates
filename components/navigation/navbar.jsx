156976b4d826cd5cf9e747e3666d426968613a68
kibrituss <46405056+kibrituss@users.noreply.github.com>
2023-07-16 11:20:32 +0300

    Update navbar.jsx

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -3,8 +3,8 @@ import styles from "../../styles/Navbar.module.css";
 export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
-			<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
-				<img className={styles.alchemy_logo} src="/cw3d-logo.png"></img>
+			<a href="https://www.kibrit.com.tr" target={"_blank"}>
+				<img className={styles.alchemy_logo} src="https://www.kibrit.com.tr/wp-content/uploads/2020/08/Kibrit_logo_3D-Animation-Studios.png"></img>
 			</a>
 			<ConnectButton></ConnectButton>
 		</nav>
